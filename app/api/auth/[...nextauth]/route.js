bd1dbc46920b3307481a3b220bf3f53e6fc96c47
Sebastian <sebastianrnajleok@gmail.com>
2025-07-03 20:58:23 -0300

    AppRunningOk

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,6 +1,6 @@
 
 import NextAuth from 'next-auth';
-import { authOptions } from '@/lib/auth'; // Adjust the import path as per your file structure
+import { authOptions } from '/lib/auth'; // Adjust the import path as per your file structure
 
 const handler = NextAuth(authOptions);
 
